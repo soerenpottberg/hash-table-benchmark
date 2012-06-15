3639abe0a053807d1a155d7b9002515cee03ff7d
Soeren Pottberg <soerenpottberg@web.de>
2012-06-15 13:46:47 +0200

    Vollstaendigkeitspruefung der Sondierungsfunktion

--- src/test/QuadraticProbingFunctionTest.java
+++ src/test/QuadraticProbingFunctionTest.java
@@ -1,5 +1,10 @@
 package test;
 
+import hashtable.HashTable;
+
+import java.util.Arrays;
+import java.util.TreeSet;
+
 import org.junit.Test;
 
 import probingfunction.IProbingFunction;
@@ -18,6 +23,30 @@ public class QuadraticProbingFunctionTest extends TestCase {
 		assertEquals(-4, probingFunction.probe(0, 0, 4));
 		assertEquals( 9, probingFunction.probe(0, 0, 5));
 		assertEquals(-9, probingFunction.probe(0, 0, 6));
+		
+	}
+	
+	@Test public void testProbe2() {
+		
+		IProbingFunction probingFunction = new QuadraticProbingFunction();
+		
+		int storageSize = 103;
+		int hashCode = 25;
+		int firstHashCode = hashCode;
+		
+		TreeSet<Integer> set = new TreeSet<Integer>();
+		set.add(hashCode);
+		
+		int tries = 0;
+		while(tries < 1*storageSize - 1) {
+			tries ++;
+			hashCode = HashTable.calculateHashTableIndex(firstHashCode + probingFunction.probe(0, storageSize, tries), storageSize);
+			set.add(hashCode);
+		}
+		
+		for (int i = 0; i < storageSize; i++) {
+			assertTrue("Zahl " + i + " nicht enthalten.", set.contains(i));
+		}
 		
 	}
 	
