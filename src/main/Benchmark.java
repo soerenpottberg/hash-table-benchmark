95544e77ffcfe4cd9659fd883afd42d62c7015f2
Soeren Pottberg <soerenpottberg@web.de>
2012-06-15 15:12:48 +0200

    Theoretische Werte zum Benchmark hinzugefuegt. Refactoring. StorageSize vergroessert

--- src/main/Benchmark.java
+++ src/main/Benchmark.java
@@ -28,12 +28,27 @@ public class Benchmark {
 	 * 83, 87, 91, 95, 99, *103*, *107*, 111, 115, 119, 123, *127*, *131*, 135, *139*, 143, 147, *151*,
 	 * 155, 159, 163, 167, 171, 175, 179, 183, 187, 191, 195, 199, 203
 	 */
-	static final int STORAGE_SIZE = 103;
+	static final int STORAGE_SIZE = 1019; // 103
 
 	/**
 	 * Füllstände
 	 */
 	static double[] betas = {0.5, 0.9, 0.95, 1};
+	
+	static final int LINEAR_PROBING_HASH_TABLE    = 0;
+	static final int QUADRATIC_PROBING_HASH_TABLE = 1;
+	static final int DOUBLE_HASHING_HASH_TABLE    = 2;
+	
+	
+	static double[][] theoreticalComplexitiesForExistingKeys =
+		{{1.5,  5.5,  10.5, Double.POSITIVE_INFINITY},  // LinearProbingHashTable
+		 {1.44, 2.85, 3.52, Double.POSITIVE_INFINITY},  // QuadraticProbingHashTable
+		 {1.39, 5.56, 3.15, Double.POSITIVE_INFINITY}}; // DoubleHashingHashTable
+	
+	static double[][] theoreticalComplexitiesForNonExistingKeys =
+		{{2.5,  50.5, 200.5, Double.POSITIVE_INFINITY},  // LinearProbingHashTable
+		 {2.19, 11.4, 22.05, Double.POSITIVE_INFINITY},  // QuadraticProbingHashTable
+		 {2,    10,   20,    Double.POSITIVE_INFINITY}}; // DoubleHashingHashTable
 	
 	static int keyCount;
 	
@@ -52,21 +67,26 @@ public class Benchmark {
 		
 		IHashTable[] hashTables = new IHashTable[3];
 		
-		for (double beta : betas) {
 			
-			hashTables[0] = new LinearProbingHashTable(STORAGE_SIZE, hashFunction);
-			hashTables[1] = new QuadraticProbingHashTable(STORAGE_SIZE, hashFunction);
-			hashTables[2] = new DoubleHashingHashTable(STORAGE_SIZE, hashFunction);
+		for (int betaIndex = 0; betaIndex < betas.length; betaIndex++) {
+			
+			double beta = betas[betaIndex];
+			
+			hashTables[LINEAR_PROBING_HASH_TABLE]    = new LinearProbingHashTable(STORAGE_SIZE, hashFunction);
+			hashTables[QUADRATIC_PROBING_HASH_TABLE] = new QuadraticProbingHashTable(STORAGE_SIZE, hashFunction);
+			hashTables[DOUBLE_HASHING_HASH_TABLE]    = new DoubleHashingHashTable(STORAGE_SIZE, hashFunction);
 			
 			keyCount = (int) (beta * STORAGE_SIZE);
 			keys                 = Universum.generateKeys(keyCount);
 			shuffledExistingKeys = Universum.shuffleKeys(keys);
 			notExistingKeys      = Universum.generateNotExistingKeys(keyCount);
 
-			for (IHashTable hashTable : hashTables) {
+			for (int hashTableIndex = 0; hashTableIndex < hashTables.length; hashTableIndex++) {
+				IHashTable hashTable = hashTables[hashTableIndex];
+				
 				System.out.println(hashTable.getClass().getSimpleName());
 				System.out.println("Beta: " + beta);
-				runTest(hashTable);
+				runTest(hashTable, theoreticalComplexitiesForExistingKeys[hashTableIndex][betaIndex], theoreticalComplexitiesForNonExistingKeys[hashTableIndex][betaIndex]);
 				System.out.println("----------------------------------------");
 			}
 			
@@ -77,9 +97,69 @@ public class Benchmark {
 	}
 
 
-	private static void runTest(IHashTable hashTable) {
+	private static void runTest(IHashTable hashTable, double theoreticalComplexityForExistingKeys, double theoreticalComplexityForNonExistingKeys) {
 
-		// Saving
+		// Speichern
+		saveEntries(hashTable);
+		
+		// Erfolgreiches Lesen
+		double complexityForExistingKeys = readWithExistingKeys(hashTable);
+		
+		complexityForExistingKeys = Math.round(complexityForExistingKeys * 100) / 100.0;
+		
+		System.out.println("Erfolgreiches Lesen:");
+		System.out.println(complexityForExistingKeys + " (" + theoreticalComplexityForExistingKeys + ")");
+		
+		hashTable.resetReadTries();
+				
+		// Erfolgloses Lesen:
+        double complexityForNonExistingKeys = readWithNonExistingKeys(hashTable);
+		
+		complexityForNonExistingKeys = Math.round(complexityForNonExistingKeys * 100) / 100.0;
+		
+		System.out.println("Erfolgloses Lesen:");
+		System.out.println(complexityForNonExistingKeys + " (" + theoreticalComplexityForNonExistingKeys + ")");
+		
+	}
+
+	/**
+	 * Liest die Einträge
+	 * @param hashTable
+	 * @return durchschnittliche Leseversuche
+	 */
+	private static double readWithNonExistingKeys(IHashTable hashTable) {
+
+		for (Integer key : notExistingKeys) {
+			hashTable.readEntry(key);
+		}
+		
+		return hashTable.getReadTries() / (double)keyCount;
+		
+	}
+
+
+	/**
+	 * Liest die Einträge
+	 * @param hashTable
+	 * @return durchschnittliche Leseversuche
+	 */
+	private static double readWithExistingKeys(IHashTable hashTable) {
+
+		for (Integer key : shuffledExistingKeys) {
+			hashTable.readEntry(key);
+		}
+
+		return hashTable.getReadTries() / (double)keyCount;
+				
+	}
+
+
+	/**
+	 * Speichert die Einträge
+	 * @param hashTable
+	 */
+	private static void saveEntries(IHashTable hashTable) {
+
 		for (Integer key : keys) {
 			HashTableEntry entry = new HashTableEntry(key, "Ben");
 			try {
@@ -88,25 +168,6 @@ public class Benchmark {
 				System.err.println(e.getMessage() + " " + hashTable.getClass().getSimpleName());
 			}
 		}
-
-		// Erfolgreiches Lesen
-		for (Integer key : shuffledExistingKeys) {
-			hashTable.readEntry(key);
-		}
-		
-		System.out.println("Erfolgreiches Lesen:");
-		System.out.println((hashTable.getReadTries()) / (float)keyCount);
-		
-		hashTable.resetReadTries();
-		
-		
-		// Erfolgloses Lesen:
-		for (Integer key : notExistingKeys) {
-			hashTable.readEntry(key);
-		}
-		
-		System.out.println("Erfolgloses Lesen:");
-		System.out.println((hashTable.getReadTries()) / (float)keyCount);
 		
 	}
 
