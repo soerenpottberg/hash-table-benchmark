4a76d2f88fce3837f3fd038bf271246dcc452d6e
Soeren Pottberg <soerenpottberg@web.de>
2012-06-16 17:52:18 +0200

    Hashtable mit separater Verkettung implementiert

---
+++ src/hashtable/HashTableEntryWithReference.java
@@ -0,0 +1,54 @@
+package hashtable;
+
+public class HashTableEntryWithReference extends HashTableEntry {
+	
+	/**
+	 * Die Referenz auf einen Weiteren Tabelleneintrag
+	 */
+	private HashTableEntryWithReference reference;
+
+	public HashTableEntryWithReference(int key, Object data, HashTableEntryWithReference reference) {
+		
+		super(key, data);
+		this.setReference(reference);
+
+	}
+	
+	/**
+	 * @param entry Der Eintrag
+	 * @param reference Die Referenz
+	 */
+    public HashTableEntryWithReference(HashTableEntry entry, HashTableEntryWithReference reference) {
+		
+		this(entry.getKey(), entry.getData(), reference);
+
+	}
+    
+    /**
+	 * @param entry Der Eintrag
+	 */
+    public HashTableEntryWithReference(HashTableEntry entry) {
+		
+    	this(entry, null);
+
+	}
+
+	/**
+	 * @return Die Referenz
+	 */
+	public HashTableEntryWithReference getReference() {
+		return reference;
+	}
+
+	/**
+	 * @param reference Die Referenz
+	 */
+	public void setReference(HashTableEntryWithReference reference) {
+		this.reference = reference;
+	}
+
+	public boolean hasReference() {
+		return reference != null;
+	}
+
+}
--- src/hashtable/SeparateChainingHashTable.java
+++ src/hashtable/SeparateChainingHashTable.java
@@ -10,21 +10,67 @@ import hashfunction.IHashFunction;
  */
 public class SeparateChainingHashTable extends HashTable {
 
+	private HashTableEntryWithReference[] hashTable;
+
 	public SeparateChainingHashTable(int storageSize, IHashFunction hashFunction) {
+		
 		super(storageSize, hashFunction);
-		// TODO Auto-generated constructor stub
+		hashTable = new HashTableEntryWithReference[storageSize];
+		
 	}
 
 	@Override
 	public void saveEntry(HashTableEntry entry) {
-		// TODO Auto-generated method stub
+		saveEntry(new HashTableEntryWithReference(entry));
+	}
+	
+	public void saveEntry(HashTableEntryWithReference entry) {
+		
+		// Berechne den Hashcode zum speichern
+		int hashCode = hashFunction.hash(entry.getKey(), storageSize);
+		
+		// Kollisionserkennung
+		saveTries++;
+		if(hashTable[hashCode] != null && hashTable[hashCode].getKey() != entry.getKey()) {
+			HashTableEntryWithReference currentEntry = hashTable[hashCode];
+			saveTries++;
+			while(currentEntry.hasReference()) {
+				currentEntry = currentEntry.getReference();
+				saveTries++;
+			}
+			currentEntry.setReference(entry);
+		} else {
+			hashTable[hashCode] = entry;
+		}
 		
 	}
 
 	@Override
 	public HashTableEntry readEntry(int key) {
-		// TODO Auto-generated method stub
-		return null;
+
+		HashTableEntryWithReference entry;
+		
+		// Berechne den Hashcode zum lesen
+		int hashCode = hashFunction.hash(key, storageSize);
+
+		// Kollisionserkennung
+		readTries++;
+		if(hashTable[hashCode] != null && hashTable[hashCode].getKey() != key) {
+			entry = hashTable[hashCode];
+			readTries++;
+			while(entry.getKey() != key && entry.hasReference()) {
+				entry = entry.getReference();
+				readTries++;
+			}
+			if(entry.getKey() != key) {
+				return null;
+			}
+		} else {
+			entry = hashTable[hashCode];
+		}
+		
+		return entry;
+		
 	}
 
 }
